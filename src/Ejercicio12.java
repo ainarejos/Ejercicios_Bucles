cdcfb585854d5377483408e8781029500b3802cb
ainarejos <ainarejos@nigul.cide.es>
2020-11-13 15:59:10 +0100

    Ejercicio12

--- src/Ejercicio12.java
+++ src/Ejercicio12.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class Ejercicio12 {
     public static void main(String[] args) {
-        int n;
+        int n=0;
         int a=0;
         int b=1;
+        int valor;
         Scanner teclado=new Scanner(System.in);
-        n=teclado.nextInt();
-        for (int i = 0; i < n; i++) {
-            int sum=b+a;
+        valor=teclado.nextInt();
+        for (int i = 0; i < valor; i++) {
+            System.out.println(a);
+            n=a+b;
             a=b;
-            b=sum;
-            System.out.println(b);
+            b=n;
         }
     }
 }
